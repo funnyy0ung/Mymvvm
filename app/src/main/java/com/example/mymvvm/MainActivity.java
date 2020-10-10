6b59f97a89d6b5c684e74412efa40b9cfa2b415e
YF <1804198137@qq.com>
2020-10-10 15:41:59 +0800

    区分出两个按钮的点击事件

--- app/src/main/java/com/example/mymvvm/MainActivity.java
+++ app/src/main/java/com/example/mymvvm/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.mymvvm;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.mymvvm.databinding.ActivityMainBinding;
 
@@ -24,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
         binding.setClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v.getId()==R.id.btn_jump){
+                if (v.getId() == R.id.btn_jump) {
                     //Toast.makeText(MainActivity.this,"点击跳转",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(MainActivity.this,HandlerActivity.class));
-                }else{
-                user.setFirstName("修改后firstName");
-                user.setLastName("修改后LastName");
+                    startActivity(new Intent(MainActivity.this, HandlerActivity.class));
+                } else if (v.getId() == R.id.btn_update) {
+                    user.setFirstName("修改后firstName");
+                    user.setLastName("修改后LastName");
                 }
             }
         });
